8626a10e5ac91350beee6c4bbce90c541c2213d1
Liana Mancini <liana.mancini@gmail.com>
2018-02-26 16:09:20 -0500

    select defender from available enemies, initialize attack function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,40 @@
 $(document).ready(function(){
     console.log("ready!");
-    var charSelection;
     var isGameOn = false;
+    var inBattle = false;
+    var charSelection, remainingCharacters, enemies, defenderSelection;
+
 
     function someFunction(event){
-        console.log(event.currentTarget.id);
+        // console.log(event.currentTarget.id);
+        var target = event.currentTarget;
+
+        // Select character and define enemies at beginning of game
         if (!isGameOn){
             isGameOn = true;
-            charSelection = $(event.currentTarget).detach();
-            $(".user-character").append(charSelected);
-                
+            charSelection = $(target).detach();
+            $(".user-character").append(charSelection);
+
+            remainingCharacters = $(".char-selection-area").children();
+            enemies = $(remainingCharacters).detach();
+            $(".enemies").append(enemies);
+        } else {
+        // Select a defender from the available enemies
+            if( $(target).parent(".enemies").length && !inBattle){
+                console.log("clicked an enemy");
+                defenderSelection = $(target).detach();
+                $(".defender").append(defenderSelection);
+                inBattle = true;
+            }
+        }
+    }
+
+    function attack(){
+        if (inBattle){
+            console.log("attack!");
         }
     }
 
     $(".character").on("click", someFunction);
+    $("button").on("click", attack);
 })
