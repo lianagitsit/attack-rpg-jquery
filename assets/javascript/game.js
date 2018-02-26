1998ba80103c65b35c96946c2b0277856c931840
Liana Mancini <liana.mancini@gmail.com>
2018-02-26 16:52:27 -0500

    refactored to dynamically create characters using object constructor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,51 @@ $(document).ready(function(){
     var inBattle = false;
     var charSelection, remainingCharacters, enemies, defenderSelection;
 
+    function Character(health, attack, counter){
 
-    function someFunction(event){
+        function create(name){
+            var charDiv = $("<div>");
+            $(charDiv).addClass("character");
+            $(charDiv).attr("id", name);
+
+            var nameSpan = $("<span>");
+            $(nameSpan).addClass("character-name");
+            $(nameSpan).text(name);
+            $(charDiv).append(nameSpan);
+
+            var charImage = $("<img>");
+            var src = "assets/images/" + name + ".jpeg";
+            $(charImage).addClass("character-img img-responsive center-block");
+            $(charImage).attr("src", src);
+            $(charDiv).append(charImage);
+
+            var hpSpan = $("<span>");
+            $(hpSpan).addClass("hit-points");
+            $(hpSpan).text(this.healthPoints);
+            $(charDiv).append(hpSpan);
+
+            $(".char-selection-area").append(charDiv);
+        }
+
+        this.healthPoints = health;
+        this.attackPower = attack;
+        this.counterAttackPower = counter;
+        this.create = create;
+    }
+
+    var baby = new Character(100, 6, 5);
+    baby.create("baby");
+
+    var bunny = new Character(120, 8, 7);
+    bunny.create("bunny");
+
+    var puppy = new Character(140, 10, 9);
+    puppy.create("puppy");
+
+    var sloth = new Character(200, 12, 11);
+    sloth.create("sloth");
+
+    function startGame(event){
         // console.log(event.currentTarget.id);
         var target = event.currentTarget;
 
@@ -35,6 +78,6 @@ $(document).ready(function(){
         }
     }
 
-    $(".character").on("click", someFunction);
+    $(".character").on("click", startGame);
     $("button").on("click", attack);
 })
