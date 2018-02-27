7070362f4a8191393f02fc81b259376ced329553
Liana Mancini <liana.mancini@gmail.com>
2018-02-26 23:01:01 -0500

    added basic attack and counter-attack abilities

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,51 +2,98 @@ $(document).ready(function(){
     console.log("ready!");
     var isGameOn = false;
     var inBattle = false;
-    var charSelection, remainingCharacters, enemies, defenderSelection;
+    var charSelection, detachedCharDiv, remainingCharacters, enemies, defenderSelection, detachedDefDiv;
 
-    function Character(health, attack, counter){
+    function Character(name, health, attack, counterAttack){
+        this.name = name;
+        this.healthPoints = health;
+        this.attackPower = attack;
+        this.counterAttackPower = counterAttack;
 
-        function create(name){
+        var baseAttackPower = this.attackPower;
+
+        // can probably get this all done in the constructor rather than in a method?
+        this.create = function create(){
             var charDiv = $("<div>");
             $(charDiv).addClass("character");
-            $(charDiv).attr("id", name);
+            $(charDiv).attr("id", this.name);
 
             var nameSpan = $("<span>");
             $(nameSpan).addClass("character-name");
-            $(nameSpan).text(name);
+            $(nameSpan).text(this.name);
             $(charDiv).append(nameSpan);
 
             var charImage = $("<img>");
-            var src = "assets/images/" + name + ".jpeg";
+            var src = "assets/images/" + this.name + ".jpeg";
             $(charImage).addClass("character-img img-responsive center-block");
             $(charImage).attr("src", src);
             $(charDiv).append(charImage);
 
             var hpSpan = $("<span>");
-            $(hpSpan).addClass("hit-points");
+            $(hpSpan).addClass("health-points");
             $(hpSpan).text(this.healthPoints);
             $(charDiv).append(hpSpan);
 
             $(".char-selection-area").append(charDiv);
+        };
+
+        this.attack = function attack(){
+            console.log(this.name + " attacks and deals " + this.attackPower + " damage!");
+            // opponent's HP reduced by player's attack power
+            switch(defenderSelection) {
+                case "baby":
+                    baby.healthPoints -= this.attackPower;
+                    $("#baby").children("span.health-points").text(baby.healthPoints);
+                    break;
+                case "bunny":
+                    bunny.healthPoints -= this.attackPower;
+                    $("#bunny").children("span.health-points").text(bunny.healthPoints);
+                    break;
+                case "puppy":
+                    puppy.healthPoints -= this.attackPower;
+                    $("#puppy").children("span.health-points").text(puppy.healthPoints);
+                    break;
+                case "sloth":
+                    sloth.healthPoints -= this.attackPower;
+                    $("#sloth").children("span.health-points").text(sloth.healthPoints);
+                    break;
+            }
+            // player's attack power increase
+            this.attackPower += baseAttackPower;
+
+        };
+
+        this.counterAttack = function(){
+            console.log(this.name + " counter-attacks and deals " + this.counterAttackPower + " damage!");
+            // player's HP reduced by opponent's counter-attack power
+            switch(charSelection) {
+                case "baby":
+                    baby.healthPoints -= this.counterAttackPower;
+                    $("#baby").children("span.health-points").text(baby.healthPoints);
+                    break;
+                case "bunny":
+                    bunny.healthPoints -= this.counterAttackPower;
+                    $("#bunny").children("span.health-points").text(bunny.healthPoints);
+                    break;
+                case "puppy":
+                    puppy.healthPoints -= this.counterAttackPower;
+                    $("#puppy").children("span.health-points").text(puppy.healthPoints);
+                    break;
+                case "sloth":
+                    sloth.healthPoints -= this.counterAttackPower;
+                    $("#sloth").children("span.health-points").text(sloth.healthPoints);
+                    break;
+            }
+
         }
 
-        this.healthPoints = health;
-        this.attackPower = attack;
-        this.counterAttackPower = counter;
-        this.create = create;
+        this.create();
     }
 
-    var baby = new Character(100, 6, 5);
-    baby.create("baby");
-
-    var bunny = new Character(120, 8, 7);
-    bunny.create("bunny");
-
-    var puppy = new Character(140, 10, 9);
-    puppy.create("puppy");
-
-    var sloth = new Character(200, 12, 11);
-    sloth.create("sloth");
+    var baby = new Character("baby", 100, 6, 5);
+    var bunny = new Character("bunny", 120, 8, 7);
+    var puppy = new Character("puppy", 140, 10, 9);
+    var sloth = new Character("sloth", 200, 12, 11);
 
     function startGame(event){
         // console.log(event.currentTarget.id);
@@ -55,8 +102,9 @@ $(document).ready(function(){
         // Select character and define enemies at beginning of game
         if (!isGameOn){
             isGameOn = true;
-            charSelection = $(target).detach();
-            $(".user-character").append(charSelection);
+            charSelection = event.currentTarget.id;
+            detachedCharDiv = $(target).detach();
+            $(".user-character").append(detachedCharDiv);
 
             remainingCharacters = $(".char-selection-area").children();
             enemies = $(remainingCharacters).detach();
@@ -65,19 +113,49 @@ $(document).ready(function(){
         // Select a defender from the available enemies
             if( $(target).parent(".enemies").length && !inBattle){
                 console.log("clicked an enemy");
-                defenderSelection = $(target).detach();
-                $(".defender").append(defenderSelection);
+                defenderSelection = event.currentTarget.id;
+                detachedDefDiv = $(target).detach();
+                $(".defender").append(detachedDefDiv);
                 inBattle = true;
             }
         }
     }
 
-    function attack(){
+    function assignAttack(){
         if (inBattle){
-            console.log("attack!");
+            console.log("I am in battle!");
+            switch(charSelection) {
+                case "baby":
+                    baby.attack();
+                    break;
+                case "bunny":
+                    bunny.attack();
+                    break;
+                case "puppy":
+                    puppy.attack();
+                    break;
+                case "sloth":
+                    sloth.attack();
+                    break;
+            }
+
+            switch(defenderSelection) {
+                case "baby":
+                    baby.counterAttack();
+                    break;
+                case "bunny":
+                    bunny.counterAttack();
+                    break;
+                case "puppy":
+                    puppy.counterAttack();
+                    break;
+                case "sloth":
+                    sloth.counterAttack();
+                    break;
+            }
         }
     }
 
     $(".character").on("click", startGame);
-    $("button").on("click", attack);
+    $("#attack-btn").on("click", assignAttack);
 })
